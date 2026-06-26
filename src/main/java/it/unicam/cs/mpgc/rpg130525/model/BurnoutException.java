b2a10d92ee6507e22aee98e0528ad74595dbe7b1
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-06-26 12:43:17 +0200

    creazione eccezione BurnoutException

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/model/BurnoutException.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.mpgc.rpg130525.model;
+
+public class BurnoutException extends RuntimeException {
+    public BurnoutException(String message) {
+        super(message);
+    }
+}
