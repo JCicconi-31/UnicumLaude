a9ce109a3d0f1c6354f19ca3e3a4c44b2b10567e
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-06-27 13:00:50 +0200

    fix: PrerequisitiNonRispettatiException è diventata checked

--- src/main/java/it/unicam/cs/mpgc/rpg130525/model/BurnoutException.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/model/BurnoutException.java
@@ -2,6 +2,6 @@ package it.unicam.cs.mpgc.rpg130525.model;
 
 public class BurnoutException extends RuntimeException {
     public BurnoutException(String message) {
-        super(message);
+        super("Burnout dello studente " + message);
     }
 }
--- src/main/java/it/unicam/cs/mpgc/rpg130525/model/PrerequisitiNonRispettatiException.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/model/PrerequisitiNonRispettatiException.java
@@ -1,7 +1,7 @@
 package it.unicam.cs.mpgc.rpg130525.model;
 
-public class PrerequisitiNonRispettatiException extends RuntimeException {
+public class PrerequisitiNonRispettatiException extends Exception {
     public PrerequisitiNonRispettatiException(String message) {
-        super(message);
+        super("Lo studente non può accedere alla seguente stanza: " + message);
     }
 }
