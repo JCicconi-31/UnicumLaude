6b35c6330f3f34d3e4b58716c6974639c469cbd5
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-06-27 12:55:42 +0200

    creazione PrerequisitiNonRispettatiException

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/model/PrerequisitiNonRispettatiException.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.mpgc.rpg130525.model;
+
+public class PrerequisitiNonRispettatiException extends RuntimeException {
+    public PrerequisitiNonRispettatiException(String message) {
+        super(message);
+    }
+}
