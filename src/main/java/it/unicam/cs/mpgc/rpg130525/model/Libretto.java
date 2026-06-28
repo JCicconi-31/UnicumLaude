3c65e5b4f95e0de9257bbe9f49b5e4458d897c63
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-06-28 16:29:36 +0200

    creazione Libretto

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/model/Libretto.java
@@ -0,0 +1,35 @@
+package it.unicam.cs.mpgc.rpg130525.model;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Libretto {
+    private final Set<EsameSuperato> esamiSuperati = new HashSet<>();
+    private int cfuOttenuti = 0;
+
+    public boolean addEsameSuperato(EsameSuperato esame){
+        if (esame == null)
+            throw new IllegalArgumentException("esame passato null");
+        if (esamiSuperati.add(esame)){
+            aggiungiCFU(esame.getEsame().getCfuAssociati());
+            return true;
+        }
+        return false;
+    }
+
+    public void aggiungiCFU(int quantita) {
+        this.cfuOttenuti += quantita;
+    }
+
+    public int getCfuOttenuti(){
+        return cfuOttenuti;
+    }
+
+    public Set<Esame> getEsamiSuperati() {
+        return esamiSuperati.stream()
+                .map(EsameSuperato::getEsame)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+}
