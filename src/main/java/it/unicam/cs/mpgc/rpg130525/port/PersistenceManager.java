d63ed0638e2340ed64f8a95190d8fc7d7918084c
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-06-29 12:57:47 +0200

    creazione Interfaccia PersistenceManager

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/port/PersistenceManager.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.mpgc.rpg130525.port;
+
+import it.unicam.cs.mpgc.rpg130525.model.StatoGioco;
+
+public interface PersistenceManager {
+    void salva(StatoGioco stato);
+    StatoGioco carica();
+}
