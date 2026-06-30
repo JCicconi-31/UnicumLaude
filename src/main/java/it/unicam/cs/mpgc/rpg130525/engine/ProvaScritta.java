5c9bd3040d4f6bd400ea6566a72f67c78a9a98e4
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-06-30 15:31:39 +0200

    aggiunta di ProvaScritta

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/engine/ProvaScritta.java
@@ -0,0 +1,39 @@
+package it.unicam.cs.mpgc.rpg130525.engine;
+
+import it.unicam.cs.mpgc.rpg130525.model.DatabaseDomande;
+import it.unicam.cs.mpgc.rpg130525.model.Domanda;
+import it.unicam.cs.mpgc.rpg130525.model.Esame;
+import it.unicam.cs.mpgc.rpg130525.model.StatoGioco;
+import it.unicam.cs.mpgc.rpg130525.port.GameInput;
+import it.unicam.cs.mpgc.rpg130525.port.GameView;
+
+public class ProvaScritta implements FaseEsame {
+    private final DatabaseDomande database;
+    private final GameInput input;
+    private final CalcolatoreDanno calcolatoreDanno;
+
+    public ProvaScritta(DatabaseDomande database, GameInput input, CalcolatoreDanno calcolatoreDanno) {
+        if (database == null || input == null || calcolatoreDanno == null)
+            throw new IllegalArgumentException("parametri null");
+        this.database = database;
+        this.input = input;
+        this.calcolatoreDanno = calcolatoreDanno;
+    }
+
+    @Override
+    public boolean esegui(StatoGioco stato, GameView view) {
+        Esame esame = stato.getPosizioneCorrente().getEsame();
+        Domanda domanda = database.estraiCasuale(esame.getNomeCorso());
+        view.mostraMessaggio(domanda.getTesto());
+        int scelta = input.chiediRisposta(domanda);
+        if (domanda.isCorretta(scelta)) {
+            view.mostraMessaggio("Risposta corretta!");
+            return true;
+        }
+        int danno = calcolatoreDanno.dannoSubito(esame.getProfessore(), stato.getStudente());
+        stato.getStudente().subisciDanno(danno);
+        view.mostraMessaggio("Risposta errata: subisci " + danno + " stress");
+        return false;
+    }
+
+}
