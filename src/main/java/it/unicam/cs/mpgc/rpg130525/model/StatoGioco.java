4fee1942b2a9c93500b1c877c162f7bee1f82356
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-06-30 11:49:56 +0200

    fix: posizioneCorrente non è più final e aggiunta metodo spostaIn()

--- src/main/java/it/unicam/cs/mpgc/rpg130525/model/StatoGioco.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/model/StatoGioco.java
@@ -2,7 +2,7 @@ package it.unicam.cs.mpgc.rpg130525.model;
 
 public class StatoGioco {
     private final Studente studente;
-    private final Stanza posizioneCorrente;
+    private Stanza posizioneCorrente;
 
     public StatoGioco(Studente studente, Stanza stanzaAttuale) {
         if (studente == null || stanzaAttuale == null)
@@ -10,6 +10,12 @@ public class StatoGioco {
         this.studente = studente;
         this.posizioneCorrente = stanzaAttuale;
     }
+    public void spostaIn(Stanza nuova) {
+        if (nuova == null)
+            throw new IllegalArgumentException("posizione nulla");
+        this.posizioneCorrente = nuova;
+    }
+
     public Studente getStudente() { return studente; }
     public Stanza getPosizioneCorrente() { return posizioneCorrente; }
 }
