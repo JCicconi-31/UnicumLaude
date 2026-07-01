409346e0178aa94e4159e248798823c5c6c9c4cb
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-07-01 12:44:26 +0200

    creazione GameStateDto

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/persistence/GameStateDto.java
@@ -0,0 +1,22 @@
+package it.unicam.cs.mpgc.rpg130525.persistence;
+
+import java.util.List;
+import java.util.Map;
+
+public record GameStateDto(
+        // attributi statici
+        String nome,
+        String cognome,
+        int intelligenzaBase,
+        int resilienzaBase,
+        int saluteMentaleMaxBase,
+        int moneteBase,
+        String tipoCarriera,
+        // attributi dinamici
+        int saluteMentaleAttuale,
+        String nomeStanzaCorrente,
+        List<EsameSuperatoDto> esamiSuperati,
+        Map<String, Integer> inventario
+) {
+    public record EsameSuperatoDto(int codiceCorso, int voto) {}
+}
