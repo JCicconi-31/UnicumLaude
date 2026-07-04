a34ce9c9a8fd624d54046e7eaba092a98d704644
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-07-04 16:42:34 +0200

    aggiunta di getDescrizione() e getNome() in CareerStrategy

--- src/main/java/it/unicam/cs/mpgc/rpg130525/model/CareerStrategy.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/model/CareerStrategy.java
@@ -3,4 +3,11 @@ package it.unicam.cs.mpgc.rpg130525.model;
 public interface CareerStrategy {
     int modificatoreHpMax();
     int modificatoreMoneteIniziali();
+
+    String getNome();
+
+    default String getDescrizione() {
+        return String.format("HP max %+d, monete iniziali %+d",
+                modificatoreHpMax(), modificatoreMoneteIniziali());
+    }
 }
