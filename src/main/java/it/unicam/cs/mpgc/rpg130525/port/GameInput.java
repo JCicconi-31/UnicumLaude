c28c0541e9b1953f4f1dcd16530f967cd5b93719
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-07-04 16:46:09 +0200

    aggiunta scegli e chidiTesto in GameInput

--- src/main/java/it/unicam/cs/mpgc/rpg130525/port/GameInput.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/port/GameInput.java
@@ -2,6 +2,13 @@ package it.unicam.cs.mpgc.rpg130525.port;
 
 import it.unicam.cs.mpgc.rpg130525.model.Domanda;
 
+import java.util.List;
+
 public interface GameInput {
+
     int chiediRisposta(Domanda domanda);
+
+    int scegli(String titolo, List<String> opzioni);
+
+    String chiediTesto(String prompt);
 }
