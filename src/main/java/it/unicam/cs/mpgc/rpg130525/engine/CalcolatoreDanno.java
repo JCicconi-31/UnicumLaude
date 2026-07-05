95fa91e11e17a0333a130ef683bd3e988ac81569
Jacopo Cicconi <jacopocicconi.work@gmail.com>
2026-07-05 17:10:55 +0200

    eliminazione NemicoMinore.java

--- src/main/java/it/unicam/cs/mpgc/rpg130525/engine/CalcolatoreDanno.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130525/engine/CalcolatoreDanno.java
@@ -2,7 +2,6 @@ package it.unicam.cs.mpgc.rpg130525.engine;
 
 import it.unicam.cs.mpgc.rpg130525.model.Studente;
 import it.unicam.cs.mpgc.rpg130525.model.Professore;
-import it.unicam.cs.mpgc.rpg130525.model.NemicoMinore;
 
 public class CalcolatoreDanno {
     public int dannoInflitto(Studente studente) {
@@ -14,17 +13,9 @@ public class CalcolatoreDanno {
     public int dannoSubito(Professore professore, Studente studente) {
         if (professore == null || studente == null)
             throw new IllegalArgumentException("impossibile calcolare danno subito su una o più entity null");
-        if (studente.getSaluteMentale() > 0){
+        if (studente.getSaluteMentale() > 0) {
             return Math.max(1, professore.getDifficolta() - studente.getResilienzaEffettiva());
         }
         return 0;
-    }
-
-    public int dannoSubito(NemicoMinore nemico, Studente studente) {
-        if (nemico == null)
-            throw new IllegalArgumentException("nemico  null");
-        if (studente == null)
-            throw new IllegalArgumentException("studente null");
-        return Math.max(1, nemico.getDannoStress() - studente.getResilienzaEffettiva());
     }
 }
